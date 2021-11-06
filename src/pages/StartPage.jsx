a5a92884daa8c170f6ae96a4c163f5e6c3900968
skymirs <kadetka274@gmail.com>
2021-11-06 15:09:38 +0400

    first step

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './startPage.css';
 const StartPage = () => {
 
-
+alert('Внимание! данное приложение предназначено для мобильных устройств, на остальных устройствах интерфейс может отображаться не корректно!')
 
     return (
         <div className="container">
