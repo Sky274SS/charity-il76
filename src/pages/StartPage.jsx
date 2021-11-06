c5b3a0335d27b816b065769e8f67e098da5af1af
skymirs <kadetka274@gmail.com>
2021-11-06 14:58:11 +0400

    first step

---
+++ src/pages/StartPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './startPage.css';
+const StartPage = () => {
+
+
+
+    return (
+        <div className="container">
+            <div>hello</div>
+        </div>
+    );
+};
+
+export default StartPage;
